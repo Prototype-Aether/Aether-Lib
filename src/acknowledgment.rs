cf01ced76ef1618694bb0e182b5a4d25b2fc870f
Anish Sharma <aneeshsharma@outlook.com>
2021-10-18 18:07:03 +0530

    implemented ack check

--- src/acknowledgment.rs
+++ src/acknowledgment.rs
@@ -1,5 +1,44 @@
+use std::collections::HashMap;
+
 // Acknowledgments received
-pub struct AcknowledgmentCheck {}
+pub struct AcknowledgmentCheck {
+    begin: u32,
+    list: HashMap<u32, bool>,
+}
+
+impl AcknowledgmentCheck {
+    pub fn new(begin: u32) -> AcknowledgmentCheck {
+        AcknowledgmentCheck {
+            begin,
+            list: HashMap::new(),
+        }
+    }
+
+    fn update_begin(&mut self) {
+        while self.check(&(self.begin + 1)) {
+            self.list.remove(&self.begin);
+            self.begin += 1;
+        }
+    }
+
+    pub fn insert(&mut self, ack: u32) {
+        if ack > self.begin {
+            self.list.insert(ack, true);
+        }
+        self.update_begin();
+    }
+
+    pub fn check(&self, ack: &u32) -> bool {
+        if *ack <= self.begin {
+            return true;
+        }
+
+        match self.list.get(ack) {
+            None => false,
+            Some(_) => true,
+        }
+    }
+}
 
 // Acknowledgments to be sent
 pub struct AcknowledgmentList {}
--- src/lib.rs
+++ src/lib.rs
@@ -6,9 +6,46 @@ mod packet_test {
     use crate::packet;
 
     #[test]
-    fn ack_test() {
+    fn range_test() {
         let pack = packet::Packet::new(0, 0);
         assert!(pack.ack_begin <= pack.ack_end.into());
         assert!(pack.miss_count as u32 <= (pack.ack_end as u32 - pack.ack_begin));
+    }
+}
+
+#[cfg(test)]
+mod ack_test {
+    use crate::acknowledgment::AcknowledgmentCheck;
+
+    #[test]
+    fn false_positive() {
+        let values = [16, 1024, 99, 45];
+
+        let check = [19, 32, 63, 6000];
+
+        let mut ack_check = AcknowledgmentCheck::new(0);
+
+        for v in values {
+            ack_check.insert(v);
+        }
+
+        for c in check {
+            assert!(!ack_check.check(&c));
+        }
+    }
+
+    #[test]
+    fn true_negatives() {
+        let values = [16, 1024, 99, 45];
+
+        let mut ack_check = AcknowledgmentCheck::new(0);
+
+        for v in values {
+            ack_check.insert(v);
+        }
+
+        for c in values {
+            assert!(ack_check.check(&c));
+        }
     }
 }
