bb89f6e45fb8fa97ef2d0c276f23cfd0d8374ac7
Hu4k3n <arjun.syam@gmail.com>
2021-10-18 19:38:20 +0530

    added compiling of data into vector u8

--- src/packet.rs
+++ src/packet.rs
@@ -27,8 +27,64 @@ impl Packet {
     }
 
     // compile the packet structure into raw bytes
-    pub fn compile() -> Vec<u8> {
-        Vec::new()
+    fn compile_u32(nu32: u32) -> Vec<u8> {
+        let mut u32_vec = Vec::<u8>::new();
+        u32_vec.push((nu32 >> 24) as u8);
+        u32_vec.push((nu32 >> 16) as u8);
+        u32_vec.push((nu32 >> 8) as u8);
+        u32_vec.push(nu32 as u8);
+        u32_vec
+    }
+    pub fn compile(self) -> Vec<u8> {
+        
+        let packet_default = Packet{
+            id: self.id,
+            sequence: self.sequence,
+            ack_begin: self.ack_begin,
+            ack_end: self.ack_end,
+            miss_count: self.miss_count,
+            miss: self.miss,
+            length: self.length,
+            payload: self.payload,
+        };
+
+        // <Uncomment the below code for testing>
+        // let packet_default=Packet {
+        //     id: 8008,
+        //     sequence: 8008,
+        //     ack_begin: 0,
+        //     ack_end: 0,
+        //     miss_count: 0,
+        //     miss: vec![1,2,3,4,5,6],
+        //     length: 10,
+        //     payload: vec![1,2,3,4,5,6,7,8,9,10],
+        // };
+
+        // Vector to store final compiled packet structure
+        let mut packet_vector= Vec::<u8>::new();
+        // Packet ID converting u32 to u8(vector)
+        let slice_id = Packet::compile_u32(packet_default.id);
+        packet_vector.extend(slice_id);
+        // Packet Sequence converting u32 to u8(vector)
+        let slice_sequence = Packet::compile_u32(packet_default.sequence);
+        packet_vector.extend(slice_sequence);
+        // Packet Ack Begin converting u32 to u8(vector)
+        let slice_ack_begin = Packet::compile_u32(packet_default.ack_begin);
+        packet_vector.extend(slice_ack_begin);
+        // Packet Ack End converting u8 to u8(vector)
+        packet_vector.push(packet_default.ack_end);
+        // Packet Miss Count converting u8 to u8(vector)
+        packet_vector.push(packet_default.miss_count);
+        // Packet Miss converting u8 to u8(vector)
+        packet_vector.extend(packet_default.miss);
+        // Packet Length converting u16 to u8(vector)
+        let slice_length = Packet::compile_u16(packet_default.length);
+        packet_vector.extend(slice_length);
+        // Packet Payload converting u8 to u8(vector)
+        packet_vector.extend(packet_default.payload);
+
+        // currently the packet_vector is a vector of u8 but we have to convert into string and then into bytes
+        packet_vector
     }
 }
 
