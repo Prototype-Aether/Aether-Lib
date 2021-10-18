e56c25a8fd195cf5917b9e5280e57c703bae3e60
Anish Sharma <aneeshsharma@outlook.com>
2021-10-18 18:22:05 +0530

    implemented tests

--- src/lib.rs
+++ src/lib.rs
@@ -11,6 +11,20 @@ mod packet_test {
         assert!(pack.ack_begin <= pack.ack_end.into());
         assert!(pack.miss_count as u32 <= (pack.ack_end as u32 - pack.ack_begin));
     }
+
+    #[test]
+    fn compile_test() {
+        let pack = packet::Packet::new(52, 32);
+
+        let compiled = pack.compile();
+
+        let pack_out = packet::Packet::from(compiled);
+
+        assert!(pack.id == pack_out.id);
+        assert!(pack.sequence == pack_out.sequence);
+        assert!(pack.ack_begin == pack_out.ack_begin);
+        assert!(pack.ack_end == pack_out.ack_end);
+    }
 }
 
 #[cfg(test)]
