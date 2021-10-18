18dfde28fe5227483103545588b4ce1c08036494
Hu4k3n <arjun.syam@gmail.com>
2021-10-18 23:17:43 +0530

    added decoding of packet

--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,14 @@
-pub mod packet;
-pub mod 
+// pub mod packet;
+// pub mod 
 
-#[cfg(test)]
-mod packet_test {
-    use crate::packet;
+// #[cfg(test)]
+// mod packet_test {
+//     use crate::packet;
 
-    #[test]
-    fn ack_test() {
-        let pack = packet::Packet::new(0, 0);
-        assert!(pack.ack_begin <= pack.ack_end.into());
-        assert!(pack.miss_count as u32 <= (pack.ack_end as u32 - pack.ack_begin));
-    }
-}
+//     #[test]
+//     fn ack_test() {
+//         let pack = packet::Packet::new(0, 0);
+//         assert!(pack.ack_begin <= pack.ack_end.into());
+//         assert!(pack.miss_count as u32 <= (pack.ack_end as u32 - pack.ack_begin));
+//     }
+// }
