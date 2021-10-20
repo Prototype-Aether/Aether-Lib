7b0e91372829dacd5a0eafba367bd963acb4836e
Anish Sharma <aneeshsharma@outlook.com>
2021-10-20 12:13:54 +0530

    implemented ack list using hash table

--- src/acknowledgment.rs
+++ src/acknowledgment.rs
@@ -1,5 +1,12 @@
 use std::collections::HashMap;
 
+pub struct Acknowledment {
+    pub ack_begin: u32,
+    pub ack_end: u8,
+    pub miss_count: u8,
+    pub miss: Vec<u8>,
+}
+
 // Acknowledgments received
 pub struct AcknowledgmentCheck {
     begin: u32,
@@ -35,69 +42,81 @@ impl AcknowledgmentCheck {
 
         match self.list.get(ack) {
             None => false,
-            Some(_) => true,
+            Some(v) => *v,
         }
     }
-}
-
-struct Acknowledment {
-    ack: u32,
-    ttl: u32,
 }
 
 // Acknowledgments to be sent
 pub struct AcknowledgmentList {
-    miss: Vec<u8>,
-    sequence: u32,
+    list: HashMap<u8, bool>,
+    ack_begin: u32,
     ack_end: u8,
 }
 
 impl AcknowledgmentList {
-    pub fn new(sequence: u32) -> AcknowledgmentList {
+    pub fn new(ack_begin: u32) -> AcknowledgmentList {
+        let mut list: HashMap<u8, bool> = HashMap::new();
+        list.insert(0, true);
         AcknowledgmentList {
-            miss: Vec::new(),
-            sequence,
+            list,
+            ack_begin,
             ack_end: 0,
         }
     }
 
-    pub fn check(&self, ack: u32) {}
+    pub fn check(&self, ack: &u32) -> bool {
+        if *ack == self.ack_begin {
+            return true;
+        } else if self.ack_begin <= *ack && *ack <= (self.ack_begin + self.ack_end as u32) {
+            let n = (*ack - self.ack_begin) as u8;
+            return match self.list.get(&n) {
+                None => false,
+                Some(v) => *v,
+            };
+        } else {
+            return false;
+        }
+    }
 
     pub fn insert(&mut self, ack: u32) {
-        if ack < self.sequence {
+        if ack < self.ack_begin {
             panic!("ack too old");
         }
 
-        if ack > (0xff + self.sequence) {
+        if ack > (0xff + self.ack_begin) {
             panic!("ack too large");
         }
 
-        let n = (ack - self.sequence) as u8;
+        let n = (ack - self.ack_begin) as u8;
 
         if n > self.ack_end {
-            let mut new_miss: Vec<u8> = ((self.ack_end + 1)..n).collect();
-
-            self.miss.append(&mut new_miss);
-
             self.ack_end = n as u8;
         }
 
-        if n < self.ack_end {
-            let index = self.miss.iter().position(|&r| r == n).unwrap();
-            self.miss.swap_remove(index);
+        self.list.insert(n, true);
+    }
+
+    pub fn get(&self) -> Acknowledment {
+        let mut miss: Vec<u8> = Vec::new();
+
+        for i in 0..(self.ack_end + 1) {
+            match self.list.get(&i) {
+                None => miss.push(i),
+                Some(false) => miss.push(i),
+                Some(true) => (),
+            }
+        }
+
+        Acknowledment {
+            ack_begin: self.ack_begin,
+            ack_end: self.ack_end,
+            miss_count: miss.len() as u8,
+            miss,
         }
     }
 
-    pub fn get(&mut self) -> (u32, u8, u8, Vec<u8>) {
-        (
-            self.sequence,
-            self.ack_end,
-            self.miss.len() as u8,
-            self.miss.clone(),
-        )
-    }
-
     pub fn is_complete(&self) -> bool {
-        self.miss.len() == 0
+        self.get().miss_count == 0
     }
 }
