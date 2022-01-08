e30bca3be582b5be3ef52417f6480833ba736166
Anish Sharma <aneeshsharma15@outlook.com>
2022-01-08 11:40:24 +0530

    fixed infinite test

--- tests/aether_test.rs
+++ tests/aether_test.rs
@@ -10,7 +10,6 @@ mod tests {
     use aether_lib::peer::Aether;
 
     #[test]
-    #[ignore]
     pub fn aether_test() {
         let tracker_addr = SocketAddr::new(IpAddr::V4(Ipv4Addr::new(127, 0, 0, 1)), 8000);
         let aether1 = Aether::new(String::from("alice"), tracker_addr);
@@ -31,32 +30,37 @@ mod tests {
             .wait_connection(&aether1.username)
             .expect("couldn't connect");
 
+        let send_str1 = format!("Hello {}", aether2.username);
         aether1
             .send_to(
                 &aether2.username,
-                String::from(format!("Hello {}", aether2.username)).into_bytes(),
+                String::from(send_str1.clone()).into_bytes(),
             )
             .expect("unable to send to peer");
 
         let result = aether2
             .recv_from(&aether1.username)
             .expect("Unable to recv");
-        println!("Received message: {}", String::from_utf8(result).unwrap());
 
+        let result_str1 = String::from_utf8(result).unwrap();
+        println!("Received message: {}", result_str1);
+
+        let send_str2 = format!("Hello {}", aether1.username);
         aether2
             .send_to(
                 &aether1.username,
-                String::from(format!("Hello {}", aether1.username)).into_bytes(),
+                String::from(send_str2.clone()).into_bytes(),
             )
             .expect("unable to send to peer");
 
         let result = aether1
             .recv_from(&aether2.username)
             .expect("Unable to recv");
-        println!("Received message: {}", String::from_utf8(result).unwrap());
 
-        loop {
-            thread::sleep(Duration::from_secs(1));
-        }
+        let result_str2 = String::from_utf8(result).unwrap();
+        println!("Received message: {}", result_str2);
+
+        assert_eq!(result_str1, send_str1);
+        assert_eq!(result_str2, send_str2);
     }
 }
