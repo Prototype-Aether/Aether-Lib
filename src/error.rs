8fba54a69b280a68b314618f8f2c3a4dafb8f1c1
Anish Sharma <aneeshsharma15@outlook.com>
2022-02-08 13:17:12 +0530

    removed traceback and print functions

--- src/error.rs
+++ src/error.rs
@@ -6,29 +6,6 @@ pub struct AetherError {
     pub cause: Option<Box<AetherError>>,
 }
 
-impl AetherError {
-    pub fn traceback(&self) -> String {
-        let mut result: String = String::new();
-
-        result = result
-            + &format!(
-                "Error code: {}\n{}\nCaused by -\n",
-                self.code, self.description
-            );
-
-        match self.cause {
-            Some(ref error) => result += &error.traceback(),
-            None => (),
-        }
-
-        result
-    }
-
-    pub fn print(&self) {
-        println!("Traceback:\n{}", self.traceback());
-    }
-}
-
 impl Display for AetherError {
     fn fmt(&self, f: &mut Formatter<'_>) -> Result {
         write!(f, "E{}: {}", self.code, self.description)
